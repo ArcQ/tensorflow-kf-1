b91f346cfb0e097c0c9b68896d2365003812a2e2
ArcQ <eddielaw296@gmail.com>
2019-03-12 23:04:51 -0400

    feat: checkpoint algorithm

--- src/deep-learning/policy-gradients/index.js
+++ src/deep-learning/policy-gradients/index.js
@@ -1,6 +1,7 @@
 import * as tf from '@tensorflow/tfjs-node';
 import {
   mean,
+  mapAccum,
   map,
   scan,
   pipe,
@@ -18,6 +19,7 @@ export async function discountAndNormalizeRewards(episodeRewards, gamma) {
     return arr.map(v => (v - meanReward) / stdReward);
   };
   const rewardsArr = await episodeRewards.array();
+
   return pipe(
     reverse,
     scan(
@@ -32,8 +34,19 @@ export async function discountAndNormalizeRewards(episodeRewards, gamma) {
   )(rewardsArr);
 }
 
-function createTrainingNets(n) {
-  const createNet = () => [
+function connectLayers(inputLayer) {
+  return mapAccum(
+    (prevLayer, layer) => {
+      const connectedLayer = layer.apply(prevLayer);
+      return [connectedLayer, connectedLayer];
+    },
+    inputLayer,
+  );
+}
+
+
+function createTrainingNets(n, inputLayer) {
+  const createSet = () => [
     tf.layers.reLU(),
     tf.layers.batchNormalization({
       training: true,
@@ -41,23 +54,26 @@ function createTrainingNets(n) {
     }),
     tf.layers.elu(),
   ];
+
   return pipe(
     range,
-    map(createNet),
+    map(createSet),
     flatten,
+    connectLayers(inputLayer),
   )(n);
 }
 
 const NUMBER_TRAINING_NETS = 3;
 
-export function PolicyGradientNetwork(stateSize, actionSize, learningRate) {
-  const inputs = tf.variable(tf.tensor(inputPlaceholder), false, 'inputs', 'float32');
-  const actions = tf.variable(tf.tensor(inputPlaceholder), false, 'actions', 'float32');
-  const discountedEpisodeRewards = tf.variable(tf.scalar(0), true, 'discountedEpisodeRewards', 'float32');
+export function createPgNetwork(stateSize, actionSize, learningRate) {
+  // const inputs = tf.variable(tf.tensor(inputPlaceholder), false, 'inputs', 'float32');
+  // const actions = tf.variable(tf.tensor(inputPlaceholder), false, 'actions', 'float32');
+  // const discountedEpisodeRewards = tf.variable(tf.scalar(0), true, 'discountedEpisodeRewards', 'float32');
+  // const mean_reward = tf.variable(tf.scalar(0), true, 'meanReward', 'float32');
+  const inputLayer = tf.input({ shape: [stateSize] });
+  const [lastLayer] = createTrainingNets(NUMBER_TRAINING_NETS, inputLayer);
 
-  const mean_reward = tf.variable(tf.scalar(0), true, 'meanReward', 'float32');
-
-  const flatten = tf.layers.flatten();
+  const flattenLayer = tf.layers.flatten();
 
   const fcOne = tf.layers.dense({
     activation: 'elu',
@@ -70,24 +86,40 @@ export function PolicyGradientNetwork(stateSize, actionSize, learningRate) {
     units: 15,
   });
 
+  const [logitLayer] = connectLayers(lastLayer)([
+    flattenLayer,
+    fcOne,
+    logits]);
+
+  const model = tf.model({ inputs: inputLayer, outputs: logitLayer });
+
   const optimizer = tf.train.rmsprop(learningRate);
 
-  const model = tf.sequential();
-  const trainingNets = createTrainingNets(NUMBER_TRAINING_NETS);
-  [...trainingNets,
-    flatten,
-    fcOne,
-    logits].map(layer => model.add(layer));
+  return {
+    train(inputs) {
+      const output = model(inputs);
+      const [xLogits, yLogits, scores] = tf.split(output, 3);
+      return {
+        posLogits: tf.stack([xLogits, yLogits]),
+        actionDistribution: tf.softmax(scores),
+      };
+    },
+    propagate() {
+      optimizer.minimize(() => {
+        const output = model();
+        const [xLogits, yLogits, scores] = tf.split(output, 3);
+        const posLogits = tf.stack([xLogits, yLogits]);
 
-  optimizer.minimize(() => {
-    const output = model(xs);
-    const [xLogits, yLogits, scores] = tf.split(output, 3);
+        const actionDistribution = tf.softmax(scores);
 
-    const actionDistribution = tf.softmax(scores);
-
-    const negLogProb = tf.losses.softmaxCrossEntropy();
-    const loss = tf.mean(tf.mul(negLogProb, discountedEpisodeRewards));
-    loss.data().then(l => console.log('Loss', l));
-    return loss;
-  });
+        const negLogProb = tf.losses.softmaxCrossEntropy({
+          logits: actionDistribution,
+          labels: posLogits,
+        });
+        const loss = tf.mean(tf.mul(negLogProb, discountedEpisodeRewards));
+        loss.data().then(l => console.log('Loss', l));
+        return loss;
+      })
+    }
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,20 +1,13 @@
 // const test = require('./build-node/battle_rust.js');
 // import('./wasm/battle_rust.js').then((test) => console.log(new test.LevelOne()));
 import * as tf from '@tensorflow/tfjs-node';
+import { map, pair, fromPair, concat, range, flatten } from 'ramda';
+import { discountAndNormalizeRewards } from 'deep-learning/policy-gradients';
+import { choose, calcDistance } from 'math';
+
 import { startGame } from './game';
 
 // const fs = require('fs');
-const initialState = {
-  goblin: {
-    pos: [100, 100],
-  },
-  assasin: {
-    pos: [200, 200],
-  },
-  moveTargetCircle: {
-    pos: [0, 0],
-  },
-};
 
 // const curId = uuid();
 // fs.writeFile(`./data/${uuid()}`, lyrics, (err) => {
@@ -57,16 +50,47 @@ const learningRate = 0.0001;
 const numEpochs = 1000;
 const batchSize = 1000; // Each 1 is a timestep (NOT AN EPISODE) # YOU CAN CHANGE TO 5000 if you have GPU
 const gamma = 0.99;
-const training = false
+const training = false;
 
-async function testEnvironment() {
+const [ZERO, D1, D2, FAR] = range(4);
+
+function getDistance(d1, d2) {
+  const dist = calcDistance(d1, d2);
+  if (dist < 200) {
+    return D2;
+  }
+  if (dist < 100) {
+    return D1;
+  }
+  if (dist < 20) {
+    return ZERO;
+  }
+  return FAR;
+}
+
+async function createGame() {
   const fps = 10;
-  const onTick = (nextState) => console.log(nextState);
-  const { nextTick, createPlayer, reset, state } = await startGame(initialState, onTick, fps);
+  const onTick = nextState => console.log(nextState);
+  const {
+    nextTick,
+    createPlayer,
+    reset,
+    state,
+  } = await startGame(initialState, onTick, fps);
   nextTick();
   const playerOne = createPlayer('P1');
   const playerTwo = createPlayer('P2');
   setTimeout(() => playerOne.setPos(220, 180), 2000);
+
+  const isEpisodeFinished = () => getDistance(state.assasin.pos, state.goblin.pos) === ZERO;
+
+  return {
+    nextTick,
+    createPlayer,
+    reset,
+    state,
+    isEpisodeFinished,
+  };
   // setInterval(() => console.log(state), 1000);
   // const episodes = 10;
   //
@@ -88,13 +112,67 @@ async function testEnvironment() {
   // });
 }
 
-function makeBatch(batchSize) {
-  let states, actions, rewards_of_episode, rewards_of_batch, discounted_rewards = [];
+function createInitialState() {
+  // TODO, state should include whether idle or moving, and if moving, point of movement
+  return {
+    goblin: {
+      pos: [100, 100],
+    },
+    assasin: {
+      pos: [200, 200],
+    },
+    moveTargetCircle: {
+      pos: [0, 0],
+    },
+  };
+}
 
-  const episodeNum = 1;
-  game.newEpisode();
-  state = game.get_state().screen_buffer;
-  state, stacked_frames = stack_frames(stacked_frames, state, True);
+function runEpisode(PgNetwork, game) {
+  const state = createInitialState();
+  const episodeState = [];
+  const episodeActions = [];
+  const episodeRewards = [];
+  let finished = false;
+  while (!finished) {
+    const { actionDistribution, posLogits } = PgNetwork.train(
+      tf.tensor(concat(state.assasin.pos, state.goblin.pos)),
+    );
+    const action = choose(posLogits, actionDistribution);
+    // TODO need to calculate reward, maybe by merging in a stream of events after action occured? 500ms?
+    const reward = game.makeAction(action);
+    finished = game.isEpisodeFinished();
+    episodeState.push(state);
+    episodeActions.push(action);
+    episodeRewards.push(reward);
+  }
+  return { episodeState, episodeActions, episodeRewards };
+}
+
+const objWithKeys = compose(
+  map((v) => pair(v, [])),
+  fromPair(),
+);
+
+function runBatch(batchSize) {
+  const batchData = objWithKeys(['states', 'actions', 'rewards', 'discountedRewards']);
+  let batchCount = 0;
+  const game = createGame();
+  const PgNetwork = createPgNetwork(stateSize, actionSize, learningRate);
+  let episodeNum = 1;
+  game.reset();
+
+  while (batchCount <= batchSize) {
+    const { episodeStates, episodeActions, episodeRewards } = runEpisode(PgNetwork, game);
+    batchData.states.push(episodeStates);
+    batchData.rewards.push(episodeRewards);
+    batchCount += tf.size(episodeRewards);
+    batchData.discountedRewards.push(
+      discountAndNormalizeRewards(episodeRewards),
+    );
+    episodeNum += 1;
+    game.reset();
+  }
+
 
   // for (let epoch = 0; epoch < 5; epoch++) {
   //   await ds.forEachAsync(({xs, ys}) => {
@@ -108,6 +186,13 @@ function makeBatch(batchSize) {
   //   console.log('Epoch', epoch);
   // }
 
+  return [
+    tf.stack(tf.tensor(batchData.states)),
+    tf.stack(tf.tensor(batchData.actions)),
+    tf.tensor(flatten(batchData.rewards)),
+    tf.tensor(flatten(batchData.discountedRewards)),
+    episodeNum,
+  ];
 }
 
 testEnvironment();
